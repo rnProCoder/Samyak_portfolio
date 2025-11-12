d82f8f5fb4c3bfc8c20d4dd0cda9eca702adcc64
aryanx16 <babreab17@gmail.com>
2025-11-12 23:06:49 +0530

    y

--- components/projects.tsx
+++ components/projects.tsx
@@ -12,7 +12,7 @@ const projects = [
     logo: "/",
       title: "ePrinter",
     description:
-      " streamlined printing solution, automating a manual campus process for over 2000 students to improve user convenience and operational efficiency, reducing user wait time by an estimated 45%.",
+      "Streamlined printing solution, automating a manual campus process for over 2000 students to improve user convenience and operational efficiency, reducing user wait time by an estimated 45%.",
     image: "/2.png",
     tags: ["React.js", "Tailwind CSS", "MongoDB","Framer Motion"],
     demoUrl: "",
